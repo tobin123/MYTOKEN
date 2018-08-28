1f3e03a97b52831c8e3a7ff94e2682fcd17e6547
tobin123 <42780453+tobin123@users.noreply.github.com>
2018-08-28 10:49:45 -0700

    Create contract.sol

---
+++ contract.sol
@@ -0,0 +1,42 @@
+#             BITCOIN GAME CRYPTODIGITAL E-COMMERCE PLATFROM (AIMS)
+1)	 A Cryptocurreny E-commerce store will be designed where Game and software developers can buy/sell their digital products with the use of BGAMES coin and other Cryptocurrencies.
+2)	 Our goal is to partner with Software developers and other recognized Game Companies.
+3)	 We will be working with businesses and companies with large numbers of games and software developers who will integrate our coin into their games as user/player rewards.
+4)	Different virtual game will also be developed on our platform where game lovers can play,win and withdraw their earnings straight into their BGAME wallet.
+Bitcoin game Marketplace will be launched in Q4. Alongside with Different games, Different partnership and different Exchange Listing, The most important advantage of using our Bitcoin Game Cryptocurreny is you can buy it online (Exchange) no matter what geographic region you reside in. Players can also fund or withdraw from their Bitcoin Game account instantaneously.
+                 Bitcoin Game Team
+BGAME has assembled an expert management team with a diverse range of Skills. These experts include experts in Cryptocurrency, virtual games development, blockchain and public relations.
+           Token sales/ Token metrics
+Bitcoin Game is going to launch Private sales to help fund the ambition to become first real online based gaming platform. The main sale is expected to launch around July. Only 10,000,000 GAMES will be sold during private sale.
+Bitcoin Game official currency is called (BGAME). Bitcoin Game will be a Cryptocurrency based on Ethereum and can be used to purchase digital products and also to play games on our platform.      
+      Token Sale has two main goals:
+	Adoption of BGAME coin.
+	Raising funds for developing and promoting the BGAME platform.
+        Token metrics
+        
+         TOKEN NAME	
+                           BITCOIN GAME
+      
+          TOKEN TICKER		
+                               BGAME
+         
+          TOTAL SUPPLY	
+                        21,000,000 BGAMES
+
+          DECIMAL	
+                                ‘’18’’
+ 
+          CONTRACT ADDRESS	
+0xa47a5e8696b47f1dd612eaad7254c4ca03938e9e
+  
+          PLATFORM	
+                Ethereum-Based token ERC20
+
+
+
+
+
+
+
+
+
